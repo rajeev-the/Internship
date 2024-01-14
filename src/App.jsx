82f405f9ff7606c342e96d0ad02b293c61b5094d
rajeev-the <rajeevranjan9560807144@gmail.com>
2024-01-14 23:40:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Nav from './Components/Nav'
+import './App.css'
+import Page1 from './Components/Page1'
+
+
+const App = () => {
+
+
+  return (
+    <div className='main'>
+      
+      
+    
+
+   
+      <Nav/>
+      <Page1/>
+    
+      
+
+
+
+
+    </div>
+  )
+}
+
+export default App
